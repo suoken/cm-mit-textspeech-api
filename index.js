a78ffe975e790de0a324ff5dc74bc728648eaf91
suoken <kensuong@gmail.com>
2018-09-16 00:23:56 -0400

    express

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-var http = require('http');
+// var http = require('http');
 
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World!');
-});
+// var server = http.createServer(function(request, response) {
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello World!");
 
-console.log('Server running at http://localhost:%d', port);
+// });
+
+// var port = process.env.PORT || 1337;
+// server.listen(port);
+
+// console.log("Server running at http://localhost:%d", port);
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello THERE'));
+
+let port = 1234;
+app.listen(port, () => console.log('Server is on' + port));
