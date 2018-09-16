bfed4ed0130968af8fb518f4b3938437c3d85252
suoken <kensuong@gmail.com>
2018-09-16 00:29:11 -0400

    express 2

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// var http = require('http');
-
-// var server = http.createServer(function(request, response) {
-
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.end("Hello World!");
-
-// });
-
-// var port = process.env.PORT || 1337;
-// server.listen(port);
-
-// console.log("Server running at http://localhost:%d", port);
-
 const express = require('express');
 
 const app = express();
