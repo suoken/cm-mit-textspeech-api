233a92a889142c184d5f484afa94884c25b6ff90
suoken <kensuong@gmail.com>
2018-09-15 23:29:04 -0400

    Hello THERE with server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
-var http = require('http');
+// var http = require('http');
 
-var server = http.createServer(function(request, response) {
+// var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello World!");
 
-});
+// });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+// var port = process.env.PORT || 1337;
+// server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+// console.log("Server running at http://localhost:%d", port);
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello THERE'));
+
+let port = 1234;
+app.listen(port, () => console.log('Server is on' + port));
