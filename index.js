cc2e823b38896de5892b0bdbc382b826985feabf
suoken <kensuong@gmail.com>
2018-09-16 00:35:59 -0400

    Added ! to there

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello THERE'));
+app.get('/', (req, res) => res.send('Hello THERE!!!!!!'));
 
 let port = 1234;
 app.listen(port, () => console.log('Server is on' + port));
